251ee1f2757806b6f2ec2e4046bee1d51bdc82eb
NecRaul <necraul@kuroneko.dev>
2024-03-06 20:37:16 +0400

    CPU/GPU/RAM modules for statusbar

--- config.h
+++ config.h
@@ -2,6 +2,9 @@
 static const Block blocks[] = {
     /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
     {"", "sb-music", 0, 11},
+    {"", "sb-cpu", 1, 12},
+    {"", "sb-gpu", 1, 13},
+    {"", "sb-ram", 1, 14},
     {"", "sb-forecast", 18000, 5},
     {"", "sb-volume", 0, 10},
     {"", "sb-clock", 60, 1},
