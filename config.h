d7cad18616e190ae019c1992bbc199defa24e372
NecRaul <necraul@kuroneko.dev>
2025-06-25 12:48:46 +0400

    Update config.h
    Decrease sb-clock's update interval to 1 seconds
    Fix comment about auto recompile to the correct path

--- config.h
+++ config.h
@@ -7,7 +7,7 @@ static const Block blocks[] = {
     {"", "sb-ram", 1, 14},
     {"", "sb-forecast", 18000, 5},
     {"", "sb-volume", 0, 10},
-    {"", "sb-clock", 60, 1},
+    {"", "sb-clock", 1, 1},
 };
 
 // Sets delimiter between status commands. NULL character ('\0') means no delimiter.
@@ -16,4 +16,5 @@ static char *delim = "\0";
 // Have dwmblocks automatically recompile and run when you edit this file in
 // vim with the following line in your vimrc/init.vim:
 
-// autocmd BufWritePost ~/Documents/Github/Repos/dwmblocks/config.h !cd ~/.local/src/dwmblocks/; sudo make install && { killall -q dwmblocks;setsid dwmblocks & }
+// autocmd BufWritePost ~/Documents/Github/Repos/dwmblocks/config.h !cd ~/Documents/Github/Repos/dwmblocks; sudo make install && { killall -q dwmblocks;setsid dwmblocks & }
+
