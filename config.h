e82fe0b8a90d8ecd2cc6c04cbec418d0fcc64402
NecRaul <necraul@kuroneko.dev>
2026-07-08 02:53:33 +0400

    feat(config): add internet block and update signals

--- config.h
+++ config.h
@@ -1,20 +1,27 @@
 // Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-    /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
-    {"", "sb-music", 0, 11},
-    {"", "sb-cpu", 1, 12},
-    {"", "sb-gpu", 1, 13},
-    {"", "sb-ram", 1, 14},
-    {"", "sb-forecast", 18000, 5},
-    {"", "sb-volume", 0, 10},
-    {"", "sb-clock", 1, 1},
+    /*Icon*/  /*Command*/  /*Update Interval*/  /*Update Signal*/
+    {"", "sb-music",    0,     11},
+    {"", "sb-cpu",      1,     12},
+    {"", "sb-gpu",      1,     13},
+    {"", "sb-ram",      1,     14},
+    {"", "sb-forecast", 18000, 5 },
+    {"", "sb-volume",   0,     10},
+    {"", "sb-internet", 60,     4 },
+    {"", "sb-clock",    1,     1 },
 };
 
 // Sets delimiter between status commands. NULL character ('\0') means no delimiter.
 static char *delim = "\0";
 
 // Have dwmblocks automatically recompile and run when you edit this file in
-// vim with the following line in your vimrc/init.vim:
-
-// autocmd BufWritePost ~/Documents/Github/Repos/dwmblocks/config.h !cd ~/Documents/Github/Repos/dwmblocks; sudo make install && { killall -q dwmblocks;setsid dwmblocks & }
-
+// neovim with the following autocmd in your init.lua:
+// vim.api.nvim_create_autocmd("BufWritePost", {
+//     pattern = vim.fn.expand("~/Documents/Github/Repos/dwmblocks/config.h"),
+//     callback = function()
+//         vim.cmd(
+//             "silent !cd ~/Documents/Github/Repos/dwmblocks && sudo make install && killall -q dwmblocks; setsid
+//             dwmblocks &"
+//         )
+//     end,
+// })
